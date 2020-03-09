27aefc68b99df782a11a5bc57bcb09541be3c32b
harideepreddy <harideepreddy2@gmail.com>
2020-03-09 19:59:52 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  userNamePattern = "^[a-zA-Z0-9_-]{6,15}$";
+   passwordPattern = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$";
+    // passwordPattern = "^[a-zA-Z0-9_-]{6,15}$";
+    // passwordPattern = "harideep";
 log(x){
 console.log(x);
+}
+log1(y){
+console.log(y);
 }
 }
